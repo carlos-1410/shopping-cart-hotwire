755de8069a73f28c942daa3f5de40325f8ea8817
Łukasz Kerl <lukasz.kerl@finalsite.com>
2025-03-23 15:36:00 +0100

    Clean-up React stuff

--- babel.config.js
+++ babel.config.js
@@ -7,23 +7,8 @@ module.exports = function (api) {
   const isProductionEnv = api.env('production')
 
   const changesOnDefault = {
-    presets: [
-      [
-        '@babel/preset-react',
-        {
-          development: !isProductionEnv,
-          useBuiltIns: true
-        }
-      ]
-    ].filter(Boolean),
-    plugins: [
-      process.env.WEBPACK_SERVE && 'react-refresh/babel',
-      isProductionEnv && ['babel-plugin-transform-react-remove-prop-types',
-        {
-          removeImport: true
-        }
-      ]
-    ].filter(Boolean),
+    presets: [].filter(Boolean),
+    plugins: [].filter(Boolean),
   }
 
   resultConfig.presets = [...resultConfig.presets, ...changesOnDefault.presets]
